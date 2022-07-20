b99e3492b66a030fc2dab231c8cf23cccba066b7
June <junetheyi@gmail.com>
2022-07-19 22:03:57 -0400

    i dont knw what im doing:'
    :

---
+++ SQLDBWithFlask/flaskrelationships/models.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+
+class Puppy(db.Model):
+
+    __tablename__ - 'puppies'
+    id = db.Column(db.Integer,primary_key=True)
