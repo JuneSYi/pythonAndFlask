bd30797b4aced9cd6a8445b044b042ff0db323f0
June <junetheyi@gmail.com>
2022-07-23 01:24:39 -0400

    more work on flask login

---
+++ UserAuthentication/userlogins/myproject/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import ValidationError
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Log in')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    username = StringField('Username',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm',message='Passwords must match!')]) #if equalTo isn't met, it sends the message
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register!')
+
+    def check_email(self,field):
+        if User.query.filter_by(email=field.data).first()
+            # if email has been registered then
+            raise ValidationError('Your email has been already registered!')
+
+    def check_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username is taken!')
