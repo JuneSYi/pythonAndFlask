8786cd4891f177bd4cfcdae1d438b65457bdf976
June <junetheyi@gmail.com>
2022-07-25 23:17:42 -0400

    added error pages, core views and templates

--- LargeCompanyBlog/puppycompanyblog/__init__.py
+++ LargeCompanyBlog/puppycompanyblog/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
